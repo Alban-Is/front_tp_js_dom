60ef8d409ce6ab66aadc26c202982f998be5b850
Alban <albanisiegas@gmail.com>
2020-09-09 17:44:37 +0200

    fin ssimple js

--- src/simple.js
+++ src/simple.js
@@ -2,7 +2,11 @@
 // Ajoutez dans un second temps une propriété de type booléen
 // Retournez l'objet
 function createObject() {
-	// votre code
+	var vehicle = Object()
+	vehicle.wheels = 4 ;
+	vehicle.doors = 3 ;
+	vehicle.hybrid = true ;
+	return vehicle ;
 }
 
 console.log(createObject());
@@ -12,7 +16,11 @@ console.log(createObject());
 // - absVal2: la valeur absolue du paramètre v2
 // - somme de v1 et v2
 function additionObject(v1, v2) {
-	// votre code
+	let lorem = new Object
+	lorem.val1 = v1
+	lorem.absval2 = Math.abs(v2)
+	lorem.somme = (v1 + v2)
+	return lorem
 }
 
 console.log(additionObject(2, 6));
@@ -21,7 +29,17 @@ console.log(additionObject(-5, -10));
 // Retournez un tableau avec uniquement des nombres impairs supérieurs à 0
 // Si le tab passé en paramètre est null, retournez un tableau vide
 function removeEvenNumbers(tab) {
-	// votre code
+	if(null != tab){
+		let arrayReturn = new Array();
+		tab.forEach(element => {
+			if (element%2 != 0 && element > 0){
+				arrayReturn.push(element)
+			}
+		});
+		return arrayReturn
+	}else{
+		return [];
+	}
 }
 
 console.log(removeEvenNumbers([]));
@@ -32,7 +50,15 @@ console.log(removeEvenNumbers([-1, 1, 2, 0, 3, 4, 12, 11]));
 // moins un élément du tableau en entrée est un nombre et
 // qu'il a une valeur négative
 function testNumNeg(tab) {
-	// votre code
+	let returnVal = false ; 
+	tab.forEach(element => {
+		if(!isNaN(element)){
+			if(element < 0 ){
+				returnVal = true ;
+			}
+		}
+	});
+	return returnVal
 }
 
 console.log(testNumNeg([1, "kiwi", true, -2]));
@@ -44,7 +70,11 @@ console.log(testNumNeg([0, 4, 8]));
 // du nombre passé en paramètre, par exemple :
 // n = 5 : [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 function multTable(n) {
-	// votre code
+	let table = new Array()
+	for(var i = 1 ; i <= 10 ; i++ ){
+		table.push(i * n)
+	}
+	return table
 }
 
 console.log("Table de multiplication de 1", multTable(1));
@@ -55,7 +85,11 @@ console.log("Table de multiplication de 5", multTable(5));
 // (jusqu'à 10 inclus) pour les nombres allant de 0 à la valeur d'un
 // paramètre max (inclus)
 function multTables(max) {
-	// votre code
+	var allTable = new Array()
+	for(var j = 0 ; j < max ; j++){
+		allTable.push(multTable(j))
+	}
+	return allTable
 }
 
 console.log(multTables(5));
@@ -81,11 +115,18 @@ const products = [
 // Vous ne devez pas créer d'objet à l'intérieur de la fonction,
 // ni utiliser de variable globale
 function howLongIsIt(obj) {
-	// votre code
+	for (const key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			let element = obj[key];
+			nCar  = element.replace(/ /g,'').length
+			obj[key] = nCar
+		}
+	}
+	return obj
 }
 
 // Vous devriez avoir à modifier cette ligne...
-let newProduct0 = howLongIsIt(products[0]);
+let newProduct0 = howLongIsIt(Object.assign({},products[0]));
 console.log(products[0]);
 /* doit afficher :
 {
@@ -106,7 +147,14 @@ console.log(newProduct0);
 // Complétez votre code avec une fonction transformant tous les objets
 // compris dans le tableau passé en paramètre de la même manière que précédemment
 function howLongIsItTab(tab) {
-	// votre code
+	let arrayObj = new Array 
+	for (const key in tab) {
+		if (tab.hasOwnProperty(key)) {
+			const element = tab[key];
+			arrayObj.push(howLongIsIt(Object.assign({},element)))
+		}
+	}
+	return arrayObj
 }
 
 // Vous devriez avoir à modifier cette ligne...
