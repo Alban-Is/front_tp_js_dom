1bb29082f77d1f40911576c510d206c90c736244
Alban <albanisiegas@gmail.com>
2020-09-09 21:26:33 +0200

    api js

--- src/api.js
+++ src/api.js
@@ -6,5 +6,38 @@
 // Ajoutez au DOM une table affichant pour tous les utilisateurs
 // les valeurs des propriétés suivantes : id, nom, nom de l'entreprise
 // La table sera créée comme nouvel enfant de la div d'id workspace
-// votre code
 
+fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => response.json())
+.then(response => traitement(JSON.stringify(response)))
+.catch(error => alert("Erreur : " + error));
+
+
+function traitement(reponse){
+    var data = JSON.parse(reponse)
+    var div_table = document.createElement('div')
+    document.body.appendChild(div_table)
+    var table = document.createElement('table')
+    table.style.height = "250px" ; 
+    table.style.display = "block" ; 
+    table.style.overflowY = 'scroll';
+    table.style.backgroundColor = "252525"
+    div_table.appendChild(table)
+    data.forEach(ligne => {
+        for (const key in ligne) {
+            if (ligne.hasOwnProperty(key)) {
+                const item = ligne[key];
+                var tr = document.createElement('tr')
+                var td_key = document.createElement('td')
+                td_key.style.fontWeight = 'bold'
+                var td_val = document.createElement('td')
+                td_key.textContent = key
+                td_val.textContent = item   
+                tr.appendChild(td_key)            
+                tr.appendChild(td_val)       
+            }
+            table.appendChild(tr)
+        }
+    });
+
+}
